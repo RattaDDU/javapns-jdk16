f50046a62c40a91d52e3848274005de4983326b1
RattaDDU <kimd.developer@gmail.com>
2016-07-21 14:25:47 +0900

    JSON map was split using comma

--- src/main/java/javapns/notification/Payload.java
+++ src/main/java/javapns/notification/Payload.java
@@ -203,7 +203,7 @@ public abstract class Payload {
 			int currentPayloadSize = getPayloadAsBytesUnchecked().length;
 			int estimatedSize = currentPayloadSize;
 			if (propertyName != null && propertyValue != null) {
-				estimatedSize += 5; // "":""
+				estimatedSize += 6; // ,"":""
 				estimatedSize += propertyName.getBytes(getCharacterEncoding()).length;
 				int estimatedValueSize = 0;
 
